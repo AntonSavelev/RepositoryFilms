9b6e5c19e9c17c4a1291322d5f09a885669d67a6
AntonSavelev <ant.savelev@gmail.com>
2017-09-24 17:12:12 +0300

    Exception

--- app/src/main/java/com/example/repositoryfilms/utils/Loader.java
+++ app/src/main/java/com/example/repositoryfilms/utils/Loader.java
@@ -3,6 +3,7 @@ package com.example.repositoryfilms.utils;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.widget.Toast;
 
 import com.example.repositoryfilms.model.AllPeople;
 import com.example.repositoryfilms.model.Character;
@@ -15,19 +16,20 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
+import okhttp3.Response;
 import retrofit2.Call;
 
 public class Loader implements Serializable {
 
+    private static final int AMOUNT_OF_ITEM_ON_PAGE = 10;
+    private static int numberOfRequest;
+    final List<Character> characters = new ArrayList<>();
     private SwapiService service;
     private PeopleDbHelper peopleDbHelper;
-
     private Set<LoadListener> listeners = new HashSet<>();
     private Handler h;
-    final List<Character> characters = new ArrayList<>();
     private int total = 0;
-    private static int nuberOfRequest;
-    private static final int AMOUNT_OF_ITEM_ON_PAGE = 10;
 
     public Loader(SwapiService service, PeopleDbHelper peopleDbHelper) {
         this.service = service;
@@ -35,7 +37,7 @@ public class Loader implements Serializable {
     }
 
     public void loadMore() {
-        nuberOfRequest = 1 + characters.size() / AMOUNT_OF_ITEM_ON_PAGE;
+        numberOfRequest = 1 + characters.size() / AMOUNT_OF_ITEM_ON_PAGE;
         if (characters.size() < total || total == 0) {
             h = new Handler() {
                 @Override
@@ -47,7 +49,7 @@ public class Loader implements Serializable {
                     notifyDataLoaded(characters);
                 }
             };
-            new MyAsyncTask().execute(nuberOfRequest);
+            new MyAsyncTask().execute(numberOfRequest);
         }
     }
 
@@ -56,7 +58,7 @@ public class Loader implements Serializable {
             characters.clear();
             characters.addAll(peopleDbHelper.getCharacters());
             notifyDataLoaded(characters);
-            nuberOfRequest = 1 + peopleDbHelper.getCharacters().size() / AMOUNT_OF_ITEM_ON_PAGE;
+            numberOfRequest = 1 + peopleDbHelper.getCharacters().size() / AMOUNT_OF_ITEM_ON_PAGE;
         }
     }
 
@@ -65,6 +67,14 @@ public class Loader implements Serializable {
         if (iterator.hasNext()) {
             LoadListener listener = iterator.next();
             listener.onCharactersLoaded(characters);
+        }
+    }
+
+    private void notifyDataLoadingError(String exception) {
+        Iterator<LoadListener> iterator = listeners.iterator();
+        if (iterator.hasNext()) {
+            LoadListener listener = iterator.next();
+            listener.onCharactersLoadingError(exception);
         }
     }
 
@@ -77,22 +87,49 @@ public class Loader implements Serializable {
         }
     }
 
-    private class MyAsyncTask extends AsyncTask<Integer, Void, Void> {
+    private class MyAsyncTask extends AsyncTask<Integer, Void, String> {
         @Override
-        protected Void doInBackground(Integer... integers) {
+        protected String doInBackground(Integer... integers) {
             Message msg;
             AllPeople allPeople;
+            String exception = null;
             List<Character> characters = new ArrayList<>();
             Call<AllPeople> call = service.getAllPeoples(integers[0]);
             try {
-                allPeople = call.execute().body();
-                characters.addAll(allPeople.getResults());
-                msg = h.obtainMessage(0, Integer.parseInt(allPeople.getCount()), 0, characters);
-                h.sendMessage(msg);
+                retrofit2.Response<AllPeople> response = call.execute();
+                if (response.isSuccessful()) {
+                    allPeople = response.body();
+                    characters.addAll(allPeople.getResults());
+                    msg = h.obtainMessage(0, Integer.parseInt(allPeople.getCount()), 0, characters);
+                    h.sendMessage(msg);
+                } else {
+                    switch (response.code()) {
+                        case 404:
+                            exception = "Not Found";
+                            break;
+                        case 408:
+                            exception = "Request Timeout";
+                            break;
+                        case 500:
+                            exception = "Server Error";
+                            break;
+                        default:
+                            exception = "Response failed";
+                            break;
+                    }
+                }
             } catch (IOException e) {
-                e.printStackTrace();
+                exception = "Request failed";
             }
-            return null;
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(String ex) {
+            super.onPostExecute(ex);
+            if (ex != null) {
+                notifyDataLoadingError(ex);
+            }
         }
     }
 }
